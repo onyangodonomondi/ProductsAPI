5ad0413215245b33a963dcdb49fec536569e8d81
onyangodonomondi <onyangodonomondi@gmail.com>
2022-09-29 15:02:49 +0300

    added features

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+# def product(request,product_id):
+#     product = Product.objects.get(pk=product_id)
+#     if product is not None:
+#         return render(request,'inventory/product.html',{'product':product})
+ 
+#     else:
+#         return HttpResponse('Product not found')
+def product(request):
+    products = Product.objects.all()
+    return render(request,'inventory/product.html',{'products':products})
